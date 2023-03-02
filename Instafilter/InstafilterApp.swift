6cf7bd50daee097e7557c2192fe4e44c39c3bd87
Joseph Yi <sendtojsph@gmail.com>
2023-03-02 11:59:34 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Joe Yi on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
